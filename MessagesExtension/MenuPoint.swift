b65d803cbd6585f6583be588d3aca434f2be9b6f
admin <admin@admin-5.local>
2017-01-24 16:30:07 +0800

    iMessageStickerApp
    This is an iMessageApp with sticker emojis for iOS 10.
    
    I used the iOS Extension…

---
+++ MessagesExtension/MenuPoint.swift
@@ -0,0 +1,18 @@
+//
+//  MenuPoint.swift
+//  Mookie
+//
+//  Created by osc_mac on 9/29/16.
+//  Copyright © 2016 com.scrape.clone. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MenuPoint: NSObject
+{
+    var nOrder : Int = 0
+    var rec : CGRect = CGRect.zero
+    var img = UIImageView()
+    var imgUrl: URL?
+}
